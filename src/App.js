38107da4d87c61e6d4b49b1ab239e2e0ee507076
kaju369 <kajalsinghal141@gmail.com>
2020-11-19 22:36:16 +0800

    springapp front-end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedCustomer: 1,
+      customerList:[]
+    }
+  }
+
+  //function which is called the first time the component loads
+  componentDidMount() {
+    
+    this.getCustomerData();
+  }
+
+  //Function to get the Customer Data from REST API
+  getCustomerData() {
+    console.log("Inside get Customer data")
+    axios.get('http://localhost:8080/api/employee').then(response => {
+        console.log("RECORDS "+response.data[0].id)
+        this.setState({customerList: response.data[0].name})
+      })
+  };
+
+  render() {
+  
+    return (<div>TESt
+      <h1>{this.state.customerList}</h1>
+    </div>);
+  }
+
 }
 
 export default App;
+
+
